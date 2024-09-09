466e67f396902b30ee57fe32e8d3582ecce5deea
Lari <165114332+LarissaIsaias@users.noreply.github.com>
2024-09-09 17:15:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express()
 const bodyParser = require('body-parser')
 const rotas = require('./routes/routesprod')
 const clientes = require('./routes/routesclien')
+const rotasAutenticacao = require('./routes/rotasAutenticador')
 
 app.use(bodyParser.json())
 
 app.use('/produtos', rotas)
 app.use('/clientes', clientes)
+app.use('/auth', rotasAutenticacao)
 
 app.listen(5000)
 

1b56a4417cbfb3455c89063f6e2c5e68656f0481
Lari <165114332+LarissaIsaias@users.noreply.github.com>
2024-09-23 17:14:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const bodyParser = require('body-parser')
 const rotas = require('./routes/routesprod')
 const clientes = require('./routes/routesclien')
 const rotasAutenticacao = require('./routes/rotasAutenticador')
+const cookieParser = require('cookie-parser')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./docs/documentacao.yaml')
 
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.use('/produtos', rotas)
 app.use('/clientes', clientes)
 app.use('/auth', rotasAutenticacao)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(5000)
 
